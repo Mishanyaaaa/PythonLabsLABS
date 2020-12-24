fd9bf435d2d8c3d8d6ec0d680f1f67e84a6fecdd
Mishanyaaaa <73399809+Mishanyaaaa@users.noreply.github.com>
2020-12-24 14:27:28 +0200

    Update settings.py

--- LAB3/django_site/settings.py
+++ LAB3/django_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+^!yz8dtnd(429(s*!!0gypms)*+5(1u@tn9*%nw7nm3ldxu-h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
